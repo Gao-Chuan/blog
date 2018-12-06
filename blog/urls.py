0e57d9d09d738c04513b7fe898b3c3eefbd9bd56
1phan <yz113@iu.edu>
2018-12-06 18:51:48 -0500

    try to make it looks better

--- blog/urls.py
+++ blog/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.contrib import staticfiles
 from django.urls import path, include
 
 urlpatterns = [
@@ -21,3 +23,4 @@ urlpatterns = [
     path('', include('home.urls')),
     path('about', include('about.urls')),
 ]
+urlpatterns += staticfiles_urlpatterns()
